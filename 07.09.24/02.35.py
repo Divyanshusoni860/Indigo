4015993391b1db0ad6babe6ba0e28aa5bfbe8bb0
Divyanshu Soni <divanshus860@gmail.com>
2024-09-13 15:14:02 +0530

    Updated 02.35.py and 02.41.py with changes

--- 07.09.24/02.35.py
+++ 07.09.24/02.35.py
@@ -15,60 +15,9 @@
 # print(l)
 
 
-# a = 12654
-# print(type(a))
+a = 12654
+print(type(a))
 
-# b  = str(a)
-# print(b)
-# print(type(b))
-
-
-#{ 
- # Driver Code Starts
-#Initial Template for Python 3
-
-# } Driver Code Ends
-#User function Template for python3
-
-def pos(n):
-    m = str(n)
-    for i in range(n):
-        m = m + (n-1)
-        n = n-1
-    return m
-        
-    
-        
-    
-def neg(n):
-    m = str(n)
-    for i in range(n):
-        m = m + (n+1)
-        n = n-1
-    return m
-
-    ##Write the code
-    
-
-#{ 
- # Driver Code Starts.
-
-
-def main():
-    testcases=int(input()) #testcases
-    while(testcases>0):
-        n = int(input())
-        if(n > 0):
-            pos(n)
-        elif(n < 0):
-            neg(n)
-        else:
-            print("already Zero",end="")
-        print()
-        testcases-=1
-        
-
-
-if __name__=='__main__':
-    main()
-# } Driver Code Ends
+b  = str(a)
+print(b)
+print(type(b))
--- 13.09.24/02.41.py
+++ 13.09.24/02.41.py
@@ -1,1 +1,5 @@
-print("Hello World")
+a = [1,25,116,48,96,75,15,355,4,9]
+print(a)
+print("Append adds a element in last")
+a.append(48)
+print("List after append is: ",a)
