2cc79031b3e91957aa426ea6a130115de2adf1b4
Divyanshu Soni <divanshus860@gmail.com>
2024-09-13 15:51:09 +0530

    list update

--- 13.09.24/02.41.py
+++ 13.09.24/02.41.py
@@ -1,13 +1,23 @@
 a = [1,25,116,48,96,75,15,355,4,9]
 print(a)
+
 print("Append adds a element in last")
 a.append(48)
 print("List after append is: ",a)
+
 print("Sorting the list")
 a.sort(reverse=True)
 print("Sorted list in reverse manner: ",a)
+
 print("Reversing a list")
 a.reverse()
 print("List after reversal: ",a)
+
 print("Display the index of given number")
 print("Index of 25 is: ",a.index(25))
+
+print("Count the no. of particular element in a list")
+print("number of times 48 present in list is: ",a.count(48))
+
+print("length shows th number of elements present in a list")
+print("Number of elemenets are :", len(a))
